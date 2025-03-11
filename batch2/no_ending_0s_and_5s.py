3dee9903fbf02abc85a4d4a7d28bedde03453c4c
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-12 06:54:59 +0800

    modified the pseudocode cuz programs got mixed up

--- batch2/no_ending_0s_and_5s.py
+++ batch2/no_ending_0s_and_5s.py
@@ -1,5 +1,9 @@
 # Prog09: Create a program that print all the numbers starting 
 # from 0 to 100 except numbers ending in zero or ending five.
 
-# Get num1 and num2 from user
-# Calculate and print the difference of num1 and num2
+# Count 0 to 100
+# If number ends in 0 or 5, don't append in list
+# Print the list
+
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter a number: "))
