73a36d7d44107f103d1b352458eae900e8607dd2
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 23:42:15 +0800

    program8 pseudocode

---
+++ batch5/num_of_char.py
@@ -0,0 +1,8 @@
+# Prog08: Create a program that ask the user to input their fullname. 
+# Print the number of characters in the input.
+# Example:
+# Input: Juan Dela Cruz
+# Output: 14
+
+# get user input
+# count num of char then print the num of char
