9c843d376f723648d4b6c6683d1846469152a165
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 22:54:40 +0800

    batch5 prog2 pseudocode

---
+++ batch5/add_zeroes_to_beginning.py
@@ -0,0 +1,10 @@
+# Prog02: Create a program that ask the user to input a number (0-1000).
+# Print the number in 6 digit format. 
+# Add zeros at the beginning to complete the 6 digit.
+# Example:
+# Input: 143
+# Output: 000143
+
+# Get user input 0 to 1000
+# Add 2 zeroes at the beginning if 4 digits, 3 if 3 digits, and so on...
+# Print the number in 6 digit format
