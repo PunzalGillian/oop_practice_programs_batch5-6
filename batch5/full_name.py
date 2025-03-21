e6320ab3f6d8b6e2174765471946f489a782c5a0
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 22:41:08 +0800

    batch5 prog1 pseudocode

---
+++ batch5/full_name.py
@@ -0,0 +1,8 @@
+#Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+#Example:
+#Input:         Juan Dela Cruz
+#Output: Juan Dela Cruz
+
+# Get the input from the user
+# Remove the spaces from the beginning
+# Print the input
