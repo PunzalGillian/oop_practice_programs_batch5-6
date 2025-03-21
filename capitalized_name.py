a41f398085e4964cb4a4219aef5a0cb8a7af83c1
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 23:08:03 +0800

    added pseudocode

---
+++ capitalized_name.py
@@ -0,0 +1,8 @@
+# Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+# Example:
+# Input: Juan Dela Cruz
+# Output: JUAN DELA CRUZ
+
+# Get user input
+# Conver the input to uppercase using built in function upper()
+# Print the input
