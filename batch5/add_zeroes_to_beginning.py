a195e96c0e142df49c58de99cbfcbdba8cd90bf3
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 22:59:12 +0800

    batch5 program2 code

--- batch5/add_zeroes_to_beginning.py
+++ batch5/add_zeroes_to_beginning.py
@@ -8,3 +8,14 @@
 # Get user input 0 to 1000
 # Add 2 zeroes at the beginning if 4 digits, 3 if 3 digits, and so on...
 # Print the number in 6 digit format
+
+num = int(input("Enter a number from (0-1000): "))
+num = str(num)
+if len(num) == 4:
+    print(f"00{num}")
+elif len(num) == 3:
+    print(f"000{num}")
+elif len(num) == 2:
+    print(f"0000{num}")
+elif len(num) == 1:
+    print(f"00000{num}")
