f9b426f8ad78e40e463c4c36bc7243545204f4d9
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 22:48:50 +0800

    deleted accidentally added folders

--- batch5/full_name.py
+++ batch5/full_name.py
@@ -1,8 +1,9 @@
-#Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
-#Example:
-#Input:         Juan Dela Cruz
-#Output: Juan Dela Cruz
+# Prog01: Create a program that ask the user to input their fullname 
+# with several space characters at the beginning.
+# Print the input without the spaces in the beginning.
+# Example:
+# Input:         Juan Dela Cruz
+# Output: Juan Dela Cruz
 
-# Get the input from the user
-# Remove the spaces from the beginning
-# Print the input
+name = str(input("Enter your full name with spaces at the beginning: "))
+print(name.lstrip())
