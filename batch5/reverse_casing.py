e037cde16b0952ef3a966a4db573091c4a2dfc77
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 23:33:19 +0800

    program6 pseudocode

---
+++ batch5/reverse_casing.py
@@ -0,0 +1,7 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: JuaN deLA cRuz
+
+# get user input
+# reverse the casing of input and print
