89e3827cabd9220b095a61b8a1ddaa9c258f84ae
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 23:31:37 +0800

    correction: program5 code

--- batch5/proper_casing.py
+++ batch5/proper_casing.py
@@ -1,7 +1,9 @@
-# Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+# Prog05: Create a program that ask the user to input their fullname in incorrect casing.
+# Print the input in proper casing.
 # Example:
 # Input: jUAn DEla CrUZ
 # Output: Juan Dela Cruz
+
 while True:
     try:
         name = str(input("Enter your name: "))
