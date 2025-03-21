1b4b161e8b5a82cff3e6aa03bd89d0fd6ae20166
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 23:37:23 +0800

    program7 pseudocode

---
+++ batch5/num_of_words.py
@@ -0,0 +1,11 @@
+# Prog07: Create a program that ask the user to 
+# input a complete statement. 
+# Print the number of words in the input.
+# Example:
+# Input: We will weather the weather whatever 
+# the weather whether we like it or not
+# Output: 14
+
+# get user input
+# split the words and count the number of words in the input
+# print the number of words
