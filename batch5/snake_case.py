328f0b038e18ab66d1ef9b3dff0f59cef0182569
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 23:48:55 +0800

    program10 pseudocode

---
+++ batch5/snake_case.py
@@ -0,0 +1,10 @@
+# Prog10: Create a program that ask the user to 
+# input their fullname in incorrect casing. 
+# Print the input in snake case.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: juan_dela_cruz
+
+# get the input from the user
+# convert the input to lower then replace space with underscore
+# print the input in snake case
