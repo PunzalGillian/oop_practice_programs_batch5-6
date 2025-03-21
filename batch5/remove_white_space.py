9c1429fc9650742efb44efbdf7b59f475b5b0716
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 23:45:17 +0800

    program9 pseudocode

---
+++ batch5/remove_white_space.py
@@ -0,0 +1,11 @@
+# Prog09: Create a program that ask the user to
+# input their fullname in incorrect casing.
+# Print the input in pascal case.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: JuanDelaCruz
+
+# get userinput
+# remove white space then use title case
+# print the result
+
