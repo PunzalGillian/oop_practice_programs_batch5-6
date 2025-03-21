1867acaf35beb78fd3b447985908b26eded0c988
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 23:27:34 +0800

    program5 pseudocode

---
+++ batch5/proper_casing.py
@@ -0,0 +1,7 @@
+# Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: Juan Dela Cruz
+
+# get user input
+# convert input to proper casing and print
