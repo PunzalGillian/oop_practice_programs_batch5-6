3903235c78f383747109995366909120fa50c65f
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 23:35:09 +0800

    program6 code

--- batch5/reverse_casing.py
+++ batch5/reverse_casing.py
@@ -1,7 +1,14 @@
-# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+# Prog06: Create a program that ask the user to input their 
+# fullname in incorrect casing.
+# Print each character of the input in reverse casing.
 # Example:
 # Input: jUAn DEla CrUZ
 # Output: JuaN deLA cRuz
 
-# get user input
-# reverse the casing of input and print
+while True:
+    try:
+        name = str(input("Enter name: "))
+        print(name.swapcase())
+        break
+    except ValueError:
+        print("Invalid input")
