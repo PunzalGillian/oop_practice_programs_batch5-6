c57613726513c8f84bd74901373da3aaf0f04cc1
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 23:20:59 +0800

    program4 pseudocode

---
+++ lowered_name.py
@@ -0,0 +1,7 @@
+# Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
+# Example:
+# Input: Juan Dela Cruz
+# Output: juan dela cruz
+
+# ask for user input
+# convert input to lowercase and print
