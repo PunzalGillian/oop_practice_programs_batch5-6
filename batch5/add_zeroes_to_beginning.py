53fa3b0dc2a5b98089d98df35676cf1bcca81ff5
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-21 23:06:43 +0800

    added valueerror

--- batch5/add_zeroes_to_beginning.py
+++ batch5/add_zeroes_to_beginning.py
@@ -5,17 +5,20 @@
 # Input: 143
 # Output: 000143
 
-# Get user input 0 to 1000
-# Add 2 zeroes at the beginning if 4 digits, 3 if 3 digits, and so on...
-# Print the number in 6 digit format
+while True: 
+    try:
+        num = int(input("Enter a number from (0-1000): "))
+        if num < 0 or num > 1000:
+            raise ValueError
+        num = str(num)
+        if len(num) == 4:
+            print(f"00{num}")
+        elif len(num) == 3:
+            print(f"000{num}")
+        elif len(num) == 2:
+            print(f"0000{num}")
+        elif len(num) == 1:
+            print(f"00000{num}")
+    except ValueError:
+        print("Invalid input. Please enter a number from 0-1000.")
 
-num = int(input("Enter a number from (0-1000): "))
-num = str(num)
-if len(num) == 4:
-    print(f"00{num}")
-elif len(num) == 3:
-    print(f"000{num}")
-elif len(num) == 2:
-    print(f"0000{num}")
-elif len(num) == 1:
-    print(f"00000{num}")
