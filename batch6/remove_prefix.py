465921b3c93addfaae6ecca9204f7d8d86644ef3
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-28 23:01:24 +0800

    prog02: remove prefix psuedocode

---
+++ batch6/remove_prefix.py
@@ -0,0 +1,8 @@
+# Prog02. removeprefix() remove the characters at the 
+# beginning of the string that matches the function parameter.
+# Create a program that do the same functionality 
+# without using removeprefix() function.
+
+# get string from user input
+# get prefix to remove
+# remove the prefix and print
