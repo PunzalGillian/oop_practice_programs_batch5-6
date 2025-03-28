76b4965e9a4a0c4ef65f92a2c92ff76173c38eaa
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-28 23:54:16 +0800

    prog8: psuedocode

---
+++ batch6/swapcase.py
@@ -0,0 +1,7 @@
+# Prog08. swapcase() reverse the casing of each of the character 
+# of the string. Create a program that do the same functionality 
+# without using swapcase() function.
+
+# get the string from the user
+# reverse the casing of each character by checking isupper() and islower()
+# print the result
