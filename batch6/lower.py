ca11476c01410eceb9b2ed331b9a628863912d48
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-28 23:14:55 +0800

    prog03: lower pseudocode

---
+++ batch6/lower.py
@@ -0,0 +1,9 @@
+# Prog03. lower() converts all characters of the string into lower case.
+# Create a program that do the same functionality without 
+# using lower() function.
+
+# get string input
+# lower the strings into lower case by going through all the chars
+# and checking if they are upper case
+# if they are, convert them to lower case
+# print the result
