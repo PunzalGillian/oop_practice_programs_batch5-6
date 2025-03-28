34a8294fe9ae9d61de510e8b4a873e21b4c6090e
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-28 23:24:09 +0800

    prog03: code

--- batch6/lower.py
+++ batch6/lower.py
@@ -2,8 +2,16 @@
 # Create a program that do the same functionality without 
 # using lower() function.
 
-# get string input
-# lower the strings into lower case by going through all the chars
-# and checking if they are upper case
-# if they are, convert them to lower case
-# print the result
+input_string = input("Enter a string: ")
+result = ""
+char = 0 
+
+while char < len(input_string):
+    if 'A' <= input_string[char] <= 'Z':
+        # ascii value of 'A' is 65 and 'a' is 97
+        result += chr(ord(input_string[char]) + 32)
+    else:
+        result += input_string[char]
+    char += 1
+
+print(result)
