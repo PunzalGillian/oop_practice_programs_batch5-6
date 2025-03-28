e3b48d8c0e62577a1aab906aea9eafd82973c1ab
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-28 23:30:03 +0800

    prog5: ends_with pseudocode

---
+++ batch6/ends_with.py
@@ -0,0 +1,7 @@
+# Prog05. endswith() check if the string end part matches the function
+# parameter. Create a program that do the same functionality 
+# without using endswith() function.
+
+# get the string and the suffix from the user
+# if the string ends with the suffix, print True
+# otherwise, print False
