bffd1f92015e27c58e5c20eb0afcb3040cf2b17c
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-28 22:49:41 +0800

    prog01: lstrip pseudocode

---
+++ batch6/lstrip_without_lstrip.py
@@ -0,0 +1,7 @@
+# Prog01. lstrip() remove the space characters at the beginning 
+# of the string. Create a program that do the same functionality 
+# without using lstrip() function.
+
+# Get string input from user
+# remove the space at the beginning of the string
+# and print the result
