ab48d38ad18a5fb88da543ce24c569d0c6968581
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-28 23:25:28 +0800

    prog04: isupper

---
+++ batch6/isupper.py
@@ -0,0 +1,8 @@
+# Prog04. isupper() check if all characters of the string is on upper
+# case. Create a program that do the same functionality 
+# without using isupper() function.
+
+# get string input
+# ceck if all characters are upper case
+# if all characters are upper case, print True
+# else, print False
