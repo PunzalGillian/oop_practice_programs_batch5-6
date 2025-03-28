c1ebc502d87e9bebd9b7c7d9136d69916ab894f8
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-28 23:37:02 +0800

    prog6: ljust pseudocode

---
+++ batch6/ljust.py
@@ -0,0 +1,10 @@
+# Prog06. ljust() add space characters at the end of the string 
+# to complete the number of characters specifies in function parameter. 
+# Create a program that do the same functionality 
+# without using ljust() function.
+
+# get string input.
+# get the character and its numbers to fill
+# create a new string, fill it with the input string
+# fill the rest of the string with the character
+# print the new string'
