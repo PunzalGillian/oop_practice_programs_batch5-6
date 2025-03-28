ded3c46f89b4e9cbcbae2584b16d03a1cb900d27
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-28 23:52:50 +0800

    prog7: cpde

--- batch6/center.py
+++ batch6/center.py
@@ -5,3 +5,15 @@
 # get string input
 # center string add space at beginning and end
 # print string
+
+input_string = input("Enter a string: ")
+width = int(input("Enter the width: "))
+
+if len(input_string) < width:
+    space_char = " " * ((width - len(input_string) // 2))
+    fill_char = "*" * ((width - len(input_string) // 2))
+else:
+    fill_char = " "
+
+print(space_char + input_string + space_char)
+print(fill_char + input_string + fill_char)
