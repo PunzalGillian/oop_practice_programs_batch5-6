eaed7b4fcad48681c174b84354019f55295b2d3f
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-28 23:57:42 +0800

    prog9: psuedocode

---
+++ batch6/capitalized.py
@@ -0,0 +1,7 @@
+# Prog09. capitalize() makes the first letter of the string, capital letter. 
+# And all other letter in small case. Create a program that do the same 
+# functionality without using capitalize() function.
+
+# get user input
+# capitalize first letter in the string and lower the rest.
+# print result
