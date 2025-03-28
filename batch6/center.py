9ca0833f3a4eda36079247dc043bb5776127e8d6
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-03-28 23:46:14 +0800

    prog7 pseudocode

---
+++ batch6/center.py
@@ -0,0 +1,7 @@
+# Prog07. center() add space characters at the beginning and at the end
+# of the string to print the string at the center. Create a program
+# that do the same functionality without using center() function.
+
+# get string input
+# center string add space at beginning and end
+# print string
