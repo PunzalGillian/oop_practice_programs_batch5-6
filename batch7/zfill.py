b4c7e11a44491bd514710bbd7005c1eb3d72ef7a
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 16:49:16 +0800

    prog7 code

---
+++ batch7/zfill.py
@@ -0,0 +1,12 @@
+# Prog07. zfill() add zero characters at the beginning of the string to 
+# complete the number of characters specifies in function parameter. 
+# Create a program that do the same functionality w
+# without using zfill() function.
+
+# get user string input
+# get user number of characters to fill
+# create a function that adds zeros to the beginning of the string
+# check if string >= number of characters to fill
+# if true return string
+# else add zeros to the beginning of the string until it reaches the number of characters
+# return the new string
