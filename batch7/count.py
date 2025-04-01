3e5ea98993dc89decd0433269ad5887131fea44e
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 16:53:06 +0800

    prog8 psuedocode

---
+++ batch7/count.py
@@ -0,0 +1,11 @@
+# Prog08. count() return how many time the function parameter 
+# appear in the string. Create a program that do the same
+# functionality without using count() function.
+
+# get user string input
+# get user character input
+# initialize a counter variable to 0
+# loop through each character in the string 
+# and check if it matches the character input
+# if it matches, increment the counter variable
+# print the counter variable
