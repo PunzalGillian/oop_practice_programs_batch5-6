ad2e4b8a9f2513141004e731206a8a5b8c720c67
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 16:45:23 +0800

    prog6 pseudocode

---
+++ batch7/rjust.py
@@ -0,0 +1,11 @@
+# Prog06. rjust() add space characters at the beginning of the string 
+# to complete the number of characters specifies in function parameter.
+# Create a program that do the same functionality without 
+# using rjust() function.
+
+# get user input
+# get width and fill character
+# check if the length of the character is less than the width
+# if so, fill the string with the fill character
+# else, fill the string with empty string
+# print the string with the fill character at the start then the string
