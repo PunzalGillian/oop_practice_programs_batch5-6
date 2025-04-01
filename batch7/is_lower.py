42bfa8166022041cd19b8e0fd869d033a04fc1a6
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 16:36:47 +0800

    prog5 pseudocode

---
+++ batch7/is_lower.py
@@ -0,0 +1,11 @@
+# Prog04. islower() check if all characters of the string is 
+# on lower case. Create a program that do the same 
+# functionality without using islower() function.
+
+# get user input
+# initialize is_lower to True
+# initialize char to 0
+# check if char is lower case
+# if char is lower case, increment char
+# else set is_lower to False and break the loop
+# print is_lower
