9b988365c062efa740400dd42fac6d45fa14ae7b
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 16:17:27 +0800

    prog01: pseudocode

---
+++ batch7/rstrip.py
@@ -0,0 +1,7 @@
+# Prog01. rstrip() remove the space characters at the end of the string.
+# Create a program that do the same functionality without 
+# using rstrip() function.
+
+# get the string from user
+# remove the space characters at the end of the string
+# print the new string
