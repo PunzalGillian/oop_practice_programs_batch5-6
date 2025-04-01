7a88d106eb3387b78384d9673813dcfd6a13fec8
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 16:30:38 +0800

    forgot to remove the pseudocode

--- batch7/remove_suffix.py
+++ batch7/remove_suffix.py
@@ -2,11 +2,6 @@
 # that matches the function parameter. Create a program that do 
 # the same functionality without using removesuffix() function.
 
-# get the string from the user and suffix to remove
-# check if end of string matches the suffix
-# if it does, remove the suffix and print the string
-# if it does not, print the string as it is
-
 input_string = input("Enter a string: ")
 suffix = input("Enter the suffix to remove: ")
 
