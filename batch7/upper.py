83971a76108a616ad5822fce242488a3b241dec0
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 16:32:37 +0800

    prog3 pseudocode

---
+++ batch7/upper.py
@@ -0,0 +1,7 @@
+# Prog03. upper() converts all characters of the string into upper 
+# case. Create a program that do the same functionality without 
+# using upper() function.
+
+# get user input
+# convert each character to upper case
+# print the result
