2c1137f3d36f03767827131b117b05d12054db25
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 17:05:29 +0800

    prog9 pseudocode

---
+++ batch7/index.py
@@ -0,0 +1,12 @@
+# Prog09. index() return the first location of the function parameter
+# in the string. Create a program that do the same functionality without
+# using index() function.
+
+# get user string input
+# get the character or string to search for
+# initialize index variable to -1
+# loop through the string
+# check if the character or string is found
+# if found, set index variable to the current position and break the loop
+# print the index variable
+# if index variable is still -1, print a message indicating not found
