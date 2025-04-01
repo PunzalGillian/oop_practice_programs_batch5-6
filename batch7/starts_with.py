5008c563bbdcecd18c1fffd634a4ff57217677ac
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 16:40:37 +0800

    prog6 pseudocode

---
+++ batch7/starts_with.py
@@ -0,0 +1,9 @@
+# Prog05. startswith() check if the string beginning part matches the 
+# function parameter. Create a program that do the same functionality
+# without using startswith() function.
+
+# get string input and prefix
+# check if string starts with prefix
+# if prefix is empty, return True
+# if string starts with prefix, return True
+# else return False
