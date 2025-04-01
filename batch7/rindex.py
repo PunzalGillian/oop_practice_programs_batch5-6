5e1d5e3c3ba59d50377b3bc5b5af6c6eb486993d
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 17:18:34 +0800

    prog10 code, prev is pseudocode

--- batch7/rindex.py
+++ batch7/rindex.py
@@ -2,8 +2,13 @@
 # in the string starting from the last character. Create a program that
 # do the same functionality without using rindex() function.
 
-# get string input and search string from user
-# check for last occurrence of search string in input string
-# if found, print index
-# if not found, print message
+input_string = input("Enter a string: ")
+search_string = input("Enter the character or string to search for: ")
+index = -1
 
+for char in range(len(input_string) - len(search_string), -1, -1):
+    if input_string[char:char + len(search_string)] == search_string:
+        index = char
+        break
+
+print(index)
