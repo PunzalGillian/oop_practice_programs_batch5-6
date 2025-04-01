1fb1241087b8fca7ccb265e714bbba9d5b34b450
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 16:30:10 +0800

    prog3 pseudocode

--- batch7/remove_suffix.py
+++ batch7/remove_suffix.py
@@ -6,3 +6,11 @@
 # check if end of string matches the suffix
 # if it does, remove the suffix and print the string
 # if it does not, print the string as it is
+
+input_string = input("Enter a string: ")
+suffix = input("Enter the suffix to remove: ")
+
+if input_string.endswith(suffix) and suffix:
+    print(input_string[:len(input_string) - len(suffix)])
+else:
+    print(input_string)
