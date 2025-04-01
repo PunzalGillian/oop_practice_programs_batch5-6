ba5b4b33e0035e2795daff83d255d77a87d9eda6
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 16:34:16 +0800

    prog4 pseudocode

--- batch7/upper.py
+++ batch7/upper.py
@@ -2,6 +2,16 @@
 # case. Create a program that do the same functionality without 
 # using upper() function.
 
-# get user input
-# convert each character to upper case
-# print the result
+input_string = input("Enter a string: ")
+result = ""
+char = 0
+
+while char < len(input_string):
+    if 'a' <= input_string[char] <= 'z':
+        # ascii value of 'a' is 97 and 'A' is 65
+        result += chr(ord(input_string[char]) - 32)
+    else: 
+        result += input_string[char]
+    char += 1
+
+print(result)
