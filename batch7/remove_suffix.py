aaee7491195ba72c547019e7d6ec6fa22dddcd87
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-01 16:27:59 +0800

    prog2 pseudocode

---
+++ batch7/remove_suffix.py
@@ -0,0 +1,8 @@
+# Prog02. removesuffix() remove the characters at the end of the string
+# that matches the function parameter. Create a program that do 
+# the same functionality without using removesuffix() function.
+
+# get the string from the user and suffix to remove
+# check if end of string matches the suffix
+# if it does, remove the suffix and print the string
+# if it does not, print the string as it is
